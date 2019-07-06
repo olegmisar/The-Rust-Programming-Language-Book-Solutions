faed0e8547d604f903a3cd9208169ca9a65b278d
olegmisar <olegmisar@gmail.com>
2019-07-06 19:58:38 +0300

    Add tests

--- ch08_english2pig_latin/src/main.rs
+++ ch08_english2pig_latin/src/main.rs
@@ -1,1 +1,37 @@
+fn english2pig_latin(s: &str) -> String {
+    String::new()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn converts_words_with_first_consonant() {
+        assert_eq!(english2pig_latin("first"), "irst-fay");
+    }
+
+    #[test]
+    fn converts_words_with_first_vowel() {
+        assert_eq!(english2pig_latin("apple"), "apple-hay");
+    }
+
+    #[test]
+    fn converts_words_with_non_alphabetical_characters() {
+        assert_eq!(english2pig_latin("five-year-old"), "ive-year-old-fay");
+    }
+
+    #[test]
+    fn converts_one_or_two_letter_words() {
+        assert_eq!(english2pig_latin("I"), "I-hay");
+        assert_eq!(english2pig_latin("me"), "e-may");
+    }
+
+    #[test]
+    fn converts_non_wordy_string_to_the_same_string() {
+        assert_eq!(english2pig_latin(""), "");
+        assert_eq!(english2pig_latin("- _"), "- _");
+    }
+}
+
 fn main() {}
