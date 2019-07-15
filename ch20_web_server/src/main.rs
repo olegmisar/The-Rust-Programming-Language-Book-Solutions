4b930f9907b521447d3370e42c7fac25da17ba22
olegmisar <olegmisar@gmail.com>
2019-07-15 16:59:23 +0300

    Add a tcp listener and a basic stream parsing

--- ch20_web_server/src/main.rs
+++ ch20_web_server/src/main.rs
@@ -1,1 +1,21 @@
-fn main() {}
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    let n_bytes_read = stream.read(&mut buffer).unwrap();
+    if n_bytes_read != 0 {
+        let data = String::from_utf8_lossy(&buffer);
+        println!("========= NEW REQUEST =========");
+        println!("{}", data);
+        println!();
+    }
+}
